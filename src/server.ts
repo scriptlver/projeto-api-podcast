ff60911d46715cd7fb043788df63d4076e26175a
scriptlver <sloowlaah1@gmail.com>
2025-07-04 20:21:40 -0300

    atts

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,9 @@
-console.log("hello")
+import * as http from 'http';
+
+const server = http.createServer(
+    (request: http.IncomingMessage, response: http.ServerResponse) => {}
+);
+
+server.listen(process.env.PORT, () => {
+    console.log("Servidor iniciado na porta 3333")
+})
