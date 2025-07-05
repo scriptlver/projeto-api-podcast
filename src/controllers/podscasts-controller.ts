48b8936a42c627aa06c9718190d047ac73f5626a
scriptlver <sloowlaah1@gmail.com>
2025-07-05 14:12:28 -0300

    amoo

--- src/controllers/podscasts-controller.ts
+++ src/controllers/podscasts-controller.ts
@@ -11,10 +11,11 @@ export const getListEpisodes = async (req : IncomingMessage, res: ServerResponse
 
 export const getFilterEpisodes = async (req : IncomingMessage, res: ServerResponse) => 
     { 
-        const queryString = req.url?.split('?p=')[1] || "";
+
         
         
-        const content = await serviceFilterEpisodes(queryString);
+        
+        const content = await serviceFilterEpisodes(req.url);
     
         res.writeHead(200, {'Content-Type': "application/json"});
         res.end(JSON.stringify(content));
