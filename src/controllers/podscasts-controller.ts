a8a7c285ed690191ec8ad6a64dd055f62931b7cb
scriptlver <sloowlaah1@gmail.com>
2025-07-04 22:27:37 -0300

    siii

--- src/controllers/podscasts-controller.ts
+++ src/controllers/podscasts-controller.ts
@@ -1,36 +1,11 @@
 import {IncomingMessage, ServerResponse} from 'http';
-
+import {serviceListEpisodes} from '../services/list-episodes-service.js';
 
 export const getListEpisodes = async (req : IncomingMessage, res: ServerResponse) => {
+    const content = await serviceListEpisodes();
    res.writeHead(200, {'Content-Type': "application/json"});
-   res.end(JSON.stringify([
-    {
-    "podcastName": "flow",
-    "episode": "CBUM – Flow #319",
-    "videoId": "pQSuQmUfS30",
-    "categories": ["saúde", "esporte", "bodybuilder"]
-  },
-  {
-    "podcastName": "flow",
-    "episode": "RUBENS BARRICHELLO – Flow #339",
-    "videoId": "4KDGTdi0V4I",
-    "categories": ["esporte", "corrida"]
-  },
-  {
-    "podcastName": "flow",
-    "episode": "LÁZARO RAMOS – Flow #290",
-    "videoId": "9MdIH33ArhY",
-    "categories": ["cultura", "cinema", "entrevista"]
-  },
-  {
-    "podcastName": "flow",
-    "episode": "FELIPE NETO – Flow #278",
-    "videoId": "vJ-LrIjBWXg",
-    "categories": ["internet", "opinião", "influenciador"]
-  }
-    
- 
-   ]));
+   res.end(JSON.stringify(content));
+
 }
 
 
---
+++ src/services/list-episodes-service.ts
@@ -0,0 +1,31 @@
+export const serviceListEpisodes = async () => {
+    const data = [
+    {
+    "podcastName": "flow",
+    "episode": "CBUM – Flow #319",
+    "videoId": "pQSuQmUfS30",
+    "categories": ["saúde", "esporte", "bodybuilder"]
+  },
+  {
+    "podcastName": "flow",
+    "episode": "RUBENS BARRICHELLO – Flow #339",
+    "videoId": "4KDGTdi0V4I",
+    "categories": ["esporte", "corrida"]
+  },
+  {
+    "podcastName": "flow",
+    "episode": "LÁZARO RAMOS – Flow #290",
+    "videoId": "9MdIH33ArhY",
+    "categories": ["cultura", "cinema", "entrevista"]
+  },
+  {
+    "podcastName": "flow",
+    "episode": "FELIPE NETO – Flow #278",
+    "videoId": "vJ-LrIjBWXg",
+    "categories": ["internet", "opinião", "influenciador"]
+  }
+    
+        ]
+
+    return data;    
+    };
