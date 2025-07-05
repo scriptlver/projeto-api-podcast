26793f835d6ac5ed2be9d313a9b83a0511e7920d
scriptlver <sloowlaah1@gmail.com>
2025-07-04 22:11:41 -0300

    siii

--- src/controllers/podscasts-controller.ts
+++ src/controllers/podscasts-controller.ts
@@ -3,10 +3,34 @@ import {IncomingMessage, ServerResponse} from 'http';
 
 export const getListEpisodes = async (req : IncomingMessage, res: ServerResponse) => {
    res.writeHead(200, {'Content-Type': "application/json"});
-   res.end({
-    name: "lavinia",
-
-   });
-};
+   res.end(JSON.stringify([
+    {
+    "podcastName": "flow",
+    "episode": "CBUM – Flow #319",
+    "videoId": "pQSuQmUfS30",
+    "categories": ["saúde", "esporte", "bodybuilder"]
+  },
+  {
+    "podcastName": "flow",
+    "episode": "RUBENS BARRICHELLO – Flow #339",
+    "videoId": "4KDGTdi0V4I",
+    "categories": ["esporte", "corrida"]
+  },
+  {
+    "podcastName": "flow",
+    "episode": "LÁZARO RAMOS – Flow #290",
+    "videoId": "9MdIH33ArhY",
+    "categories": ["cultura", "cinema", "entrevista"]
+  },
+  {
+    "podcastName": "flow",
+    "episode": "FELIPE NETO – Flow #278",
+    "videoId": "vJ-LrIjBWXg",
+    "categories": ["internet", "opinião", "influenciador"]
+  }
+    
+ 
+   ]));
+}
 
 
