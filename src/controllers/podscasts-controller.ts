f8ec244090f288328665dbe8eb3466dd3baaa90b
scriptlver <sloowlaah1@gmail.com>
2025-07-05 17:43:25 -0300

    siii

--- src/controllers/podscasts-controller.ts
+++ src/controllers/podscasts-controller.ts
@@ -1,10 +1,11 @@
 import {IncomingMessage, ServerResponse} from 'http';
 import {serviceListEpisodes} from '../services/list-episodes-service.js';
 import { serviceFilterEpisodes } from '../services/filter-episodes-service.js';
+import { StatusCode } from '../utils/status-code.js';
 
 export const getListEpisodes = async (req : IncomingMessage, res: ServerResponse) => {
     const content = await serviceListEpisodes();
-   res.writeHead(200, {'Content-Type': "application/json"});
+   res.writeHead(StatusCode.OK, {'Content-Type': "application/json"});
    res.end(JSON.stringify(content));
 
 }
@@ -17,7 +18,7 @@ export const getFilterEpisodes = async (req : IncomingMessage, res: ServerRespon
         
         const content = await serviceFilterEpisodes(req.url);
     
-        res.writeHead(200, {'Content-Type': "application/json"});
+        res.writeHead(StatusCode.OK, {'Content-Type': "application/json"});
         res.end(JSON.stringify(content));
     
     }
